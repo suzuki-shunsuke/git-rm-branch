fcb6da778c124f6e937aa2469f12a7ba09faada8
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2017-10-04 22:34:47 +0900

    feat: implement remove remote branches

--- cmds/run.go
+++ cmds/run.go
@@ -17,36 +17,7 @@ import (
 	"github.com/suzuki-shunsuke/git-rm-branch/services"
 )
 
-func runCore() error {
-	// remove branches
-	// remove local branches
-	//   git branch --merged master | grep -vE $EXCLUDED_BRANCHS | xargs -I % git branch -d %
-	// remove remote branches
-	//   git branch -r --merged master | sed -e "s/ *\(.*\) */\1/" | grep "^origin/" | sed -e "s/origin\///" | grep -v "HEAD -> " |  grep -vE $EXCLUDED_BRANCHS | xargs -I% git push --delete origin %
-	// find configuration file
-	wd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	rootDir, err := services.FindRoot(wd)
-	if err != nil {
-		return err
-	}
-	dest := filepath.Join(rootDir, services.CONFIG_FILENAME)
-	if _, err = os.Stat(dest); err != nil {
-		return errors.New("configuration file is not found")
-	}
-	// read configuration file
-	buf, err := ioutil.ReadFile(dest)
-	if err != nil {
-		return err
-	}
-	// parse configuration file
-	var cfg models.Cfg
-	err = yaml.Unmarshal(buf, &cfg)
-	if err != nil {
-		return err
-	}
+func rmLocalBranch(cfg models.Cfg) error {
 	// remove local branches
 	removedBranchCandidates := map[string]string{}
 	// list branches
@@ -78,14 +49,104 @@ func runCore() error {
 		}
 	}
 	if len(gitBranchCmdArgs) == 2 {
-		fmt.Println("no branch is removed")
+		fmt.Println("no local branch is removed")
 		return nil
 	}
 	// remove branches
 	//   git branch -d %
 	fmt.Printf("git %s\n", strings.Join(gitBranchCmdArgs, " "))
-	_, err = exec.Command("git", gitBranchCmdArgs...).Output()
+	_, err := exec.Command("git", gitBranchCmdArgs...).Output()
 	return err
+}
+
+func rmRemoteBranch(cfg models.Cfg) error {
+	// remove remote branches
+	//   git branch -r --merged master | sed -e "s/ *\(.*\) */\1/" | grep "^origin/" | sed -e "s/origin\///" | grep -v "HEAD -> " |  grep -vE $EXCLUDED_BRANCHS | xargs -I% git push --delete origin %
+	// list branches
+	//   git branch -r --merged master
+	// origin/HEAD -> origin/master
+	// origin/master
+	for remote, branches := range cfg.Remote {
+		removedBranchCandidates := map[string]string{}
+		for _, branch := range branches {
+			out, err := exec.Command("git", "branch", "-r", "--merged", branch).Output()
+			if err != nil {
+				return err
+			}
+			for _, s := range strings.Split(string(out), "\n") {
+				s = strings.TrimSpace(s)
+				if strings.HasPrefix(s, fmt.Sprintf("%s/", remote)) {
+					s = s[len(remote)+1:]
+					if !strings.Contains(s, "->") {
+						removedBranchCandidates[s] = ""
+					}
+				}
+			}
+		}
+		// exclude protected branches
+		gitPushCmdArgs := []string{"push", "--delete", remote}
+		for branch, _ := range removedBranchCandidates {
+			isRemoved := true
+			for _, b := range cfg.Remote[remote] {
+				if branch == b {
+					isRemoved = false
+					break
+				}
+			}
+			if isRemoved {
+				gitPushCmdArgs = append(gitPushCmdArgs, branch)
+			}
+		}
+		if len(gitPushCmdArgs) == 3 {
+			fmt.Println("no remote branch is removed")
+			continue
+		}
+		// remove branches
+		fmt.Printf("git %s\n", strings.Join(gitPushCmdArgs, " "))
+		_, err := exec.Command("git", gitPushCmdArgs...).Output()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func getCfg(wd string) (*models.Cfg, error) {
+	rootDir, err := services.FindRoot(wd)
+	if err != nil {
+		return nil, err
+	}
+	dest := filepath.Join(rootDir, services.CONFIG_FILENAME)
+	if _, err = os.Stat(dest); err != nil {
+		return nil, errors.New("configuration file is not found")
+	}
+	// read configuration file
+	buf, err := ioutil.ReadFile(dest)
+	if err != nil {
+		return nil, err
+	}
+	// parse configuration file
+	var cfg models.Cfg
+	err = yaml.Unmarshal(buf, &cfg)
+	return &cfg, err
+}
+
+func runCore() error {
+	// remove branches
+	// find configuration file
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	cfg, err := getCfg(wd)
+	if err != nil {
+		return err
+	}
+	err = rmLocalBranch(*cfg)
+	if err != nil {
+		return err
+	}
+	return rmRemoteBranch(*cfg)
 }
 
 func Run(c *cli.Context) error {
